f9edc5642a9d6f3d57e63baa48541c8d7156f3f3
YesunHuang <tgbus944812203@hotmail.com>
2022-03-28 10:54:16 +0800

    Update Test

--- DataGenerator/HenonMapDataGen.py
+++ DataGenerator/HenonMapDataGen.py
@@ -18,7 +18,7 @@ from SequenceDataLoader import  SeqDataLoader
 class HenonMapDataGen:
     '''Generate data of modified Henon Map'''
 
-    def __init__(self, seed:list,\
+    def __init__(self, seed:list=[],\
                         n:int=1,a:float=1.4,b:float=0.3,\
                         heavyMem:bool=True,bound:bool=-1.2,\
                         savepath:str=os.getcwd()):
@@ -33,16 +33,19 @@ class HenonMapDataGen:
         param {bound}: bound of the data
         param {savepath}: path to save the data
         '''   
-        if heavyMem:
-            assert len(seed)==n+1,'invalid seed!'
-        else:
-            assert len(seed)==2*n,'invalid seed!'    
-        self.seed=seed
+        self.heavyMem=heavyMem
         self.interval=n
+        if len(seed)==0:
+            self.random_seed()
+        else:
+            self.__seed=seed
+        if self.heavyMem:
+            assert len(self.__seed)==n+1,'invalid seed!'
+        else:
+            assert len(self.__seed)==2*n,'invalid seed!'    
         self.paramA=a
         self.paramB=b
         self.HenonFunc=lambda X1,X0:1-self.paramA*X1*X1+self.paramB*X0
-        self.HeavyMem=heavyMem
         self.savepath=savepath
         self.bound=bound
         self.__X=[]
@@ -56,10 +59,10 @@ class HenonMapDataGen:
         return {X,Y}: tuple of list in the form (X,Y)
         '''       
         self.clear_data()
-        self.__X=self.__X+self.seed
+        self.__X=self.__X+self.__seed
         self.__Y=self.__Y+[0.0]*self.interval
-        if self.HeavyMem:
-            assert size>len(self.seed), 'size not enough!'
+        if self.heavyMem:
+            assert size>len(self.__seed), 'size not enough!'
             for i in range(self.interval,size):
                 Y_next=self.HenonFunc(self.__X[i],self.__X[i-self.interval])
                 if self.interval>1:
@@ -69,12 +72,33 @@ class HenonMapDataGen:
                 self.__X.append(self.__Y[i])
             self.__X.pop()
         else:
-            assert size>len(self.seed)+self.interval, 'size not enough'
+            assert size>len(self.__seed)+self.interval, 'size not enough'
             for i in range(self.interval,size):
                 self.__Y.append(self.HenonFunc(self.__X[i],self.__X[i-self.interval]))
                 if i+self.interval<size:
                     self.__X.append(self.__Y[i])
         return np.array(self.__X),np.array(self.__Y)
+
+    def random_seed(self):
+        '''
+        name: random_seed
+        function: random the seed
+        return {seed}
+        '''
+        if self.heavyMem:
+            self.__seed=[np.random.rand()*0.1 for i in range(self.interval+1)]
+        else:
+            self.__seed=[np.random.rand()*0.1 for i in range(2*self.interval)]   
+        return self.__seed   
+
+    @property
+    def seed(self):
+        '''
+        name: seed
+        function: get the seed
+        return {seed}
+        '''
+        return self.__seed 
 
     def save_to_CSV(self,fileName:str):
         '''
@@ -97,7 +121,8 @@ class HenonMapDataGen:
         self.__X=data['X'].values.tolist()
         self.__Y=data['Y'].values.tolist()
 
-    def get_data(self):
+    @property
+    def data_as_array(self):
         '''
         name: get_data
         function: get the data
@@ -105,7 +130,8 @@ class HenonMapDataGen:
         '''
         return np.array(self.__X),np.array(self.__Y)
 
-    def get_data_as_tensor(self):
+    @property
+    def data_as_tensor(self):
         '''
         name: get_data_as_tensor
         function: get the data as tensor
