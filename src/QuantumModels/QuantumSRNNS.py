2f4e2d42289c04c0d6ef23f6742dca2c3364c10d
YesunHuang <tgbus944812203@hotmail.com>
2022-04-08 00:16:32 +0800

    Update Quantum

--- src/QuantumModels/QuantumSRNNS.py
+++ src/QuantumModels/QuantumSRNNS.py
@@ -9,14 +9,11 @@ Date: 2022-04-06 10:18:00
 '''
 
 #import everything
-from codecs import EncodedFile
-from email.policy import default
-from turtle import forward
-from typing import Dict, Tuple
-from numpy import fliplr
+from sqlalchemy import case
 import torch
 from torch import nn, pi
 from collections.abc import Callable
+import numpy as np
 import qutip as qt
 #modify path
 from GradientFreeOptimizers.CostFunc import StandardSNN
@@ -172,12 +169,12 @@ class QuantumSystemFunction:
                 WIn=normal((inputSize,len(self.inputQubits)),rescale['WIn']).requires_grad_(True)
                 params.append(WIn)
             if 'DeltaIn' in self.inactive:
-                DeltaInParam=rescale['DeltaIn']*torch.ones((inputSize,len(self.inputQubits))).detach_()
+                DeltaInParam=rescale['DeltaIn']*torch.ones(len(self.inputQubits)).detach_()
                 constants.append(DeltaInParam)
             else:
-                DeltaInParam=normal((inputSize,len(self.inputQubits)),rescale['DeltaIn']).requires_grad_(True)
+                DeltaInParam=rescale['DeltaIn']*torch.ones(len(self.inputQubits)).requires_grad_(True)
                 params.append(DeltaInParam)
-            DeltaInPad=rescale['DeltaIn']*torch.ones((inputSize,qubits-len(self.inputQubits))).detach_()
+            DeltaInPad=rescale['DeltaIn']*torch.ones(qubits-len(self.inputQubits)).detach_()
             constants.append(DeltaInPad)
             #Interaction params
             if 'J' in self.inactive:
@@ -212,18 +209,61 @@ class QuantumSystemFunction:
         return: the forward function
         '''
         self.samples,self.measEffect=samples,measEffect
+        self.sysConstants=sysConstants
 
-        def build_int_operators(J:torch.Tensor):
+        def multi_qubits_sigma(pauliBasis:list=['i']):
+            '''
+            name:build_multi_qubits_sigma
+            function: build the multi qubits sigma
+            param {pauliBasis}: the pauliBasis
+            return: the sigma
+            '''
+            sigma=[]
+            for i in range(0,len(pauliBasis)):
+                if pauliBasis[i]=='i':
+                    sigma.append(qt.qeye(2))
+                if pauliBasis[i]=='x':
+                    sigma.append(qt.sigmax())
+                if pauliBasis[i]=='y':
+                    sigma.append(qt.sigmay())
+                if pauliBasis[i]=='z':
+                    sigma.append(qt.sigmaz())
+                if pauliBasis[i]=='-':
+                    sigma.append(qt.sigmam())
+                if pauliBasis[i]=='+':
+                    sigma.append(qt.sigmap())
+            return qt.tensor(sigma)
+
+        def build_int_operators(J:torch.Tensor,qubits:int):
             '''
             name:build_int_operators
             function: build the interaction operators
             param {J}: the interaction strength
             return: the interaction operators
             '''
-            #TODO: build the interaction operators
-            pass
+            H_I=[]
+            C_ops=[]
+            assert len(J)==self.interQPairs, \
+                'The length of J is not equal to the number of interaction pairs'
+            for i in range(0,qubits):
+                pauliBasis=['i']*qubits
+                if 'Dissipation' in self.sysConstants:
+                    pauliBasis[i]='-'
+                    sigma=multi_qubits_sigma(pauliBasis)
+                    C_ops.append(np.sqrt(self.sysConstants['Dissipation'])*sigma)
+                pauliBasis[i]='x'
+                sigma=multi_qubits_sigma(pauliBasis)
+                H_I.append([self.sysConstants['Omega']/2.0,sigma])
+           
+            for i in range(0,len(self.interQPairs)):
+                pauliBasis=['i']*qubits
+                pauliBasis[self.interQPairs[i][0]]='x'
+                pauliBasis[self.interQPairs[i][1]]='x'
+                sigma=multi_qubits_sigma(pauliBasis)
+                H_I.append([J[i],sigma])
+            return H_I,C_ops
 
-        def encode_input(x:torch.Tensor,inputParams:Tuple):
+        def encode_input(x:torch.Tensor,inputParams:tuple):
             '''
             name: encodeInput
             function: encode the input
@@ -234,7 +274,7 @@ class QuantumSystemFunction:
             #TODO: encode the input into Hamiltonian and dissipation
             pass
 
-        def evolve(S:list,evolParam:Tuple):
+        def evolve(S:list,evolParam:tuple):
             '''
             name: evolve
             function: evolve the state
@@ -265,7 +305,7 @@ class QuantumSystemFunction:
             return: the output
             '''
             params,constants=weights
-            if isinstance(params,Tuple):
+            if isinstance(params,tuple):
                 params=list(params)
                 constants=list(constants)
             params,constants=params.copy(),constants.copy()
@@ -279,6 +319,7 @@ class QuantumSystemFunction:
             else:
                 DeltaInParam=params.pop(0)
             DeltaInPad=constants.pop(0)
+            qubits=DeltaInPad.shape[0]+DeltaInParam[0]
             inputParams=(WIn,DeltaInParam,DeltaInPad)
             #Interaction params
             if 'J' in self.inactive:
@@ -296,7 +337,7 @@ class QuantumSystemFunction:
                 DeltaOutParam=params.pop(0)
             S,=state
             Ys=[]
-            H_I,Co_ps=build_int_operators(J)
+            H_I,Co_ps=build_int_operators(J,qubits)
             for X in Xs:
                 H_input=encode_input(X,inputParams)
                 H=H_input+H_I
