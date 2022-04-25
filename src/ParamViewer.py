2945a444ccdc093bea7cfac747aa0ad5f004681d
YesunHuang <tgbus944812203@hotmail.com>
2022-04-25 11:14:13 +0800

    Update

--- src/ParamViewer.py
+++ src/ParamViewer.py
@@ -20,7 +20,7 @@ if __name__=='__main__':
     netSavepath=os.path.join(currentPath,'TrainedNet','Exp')
 
 if __name__=='__main__':
-    filename='QExpF.pt'
+    filename='QExpFM2.pt'
 
 
 if  __name__=='__main__':
