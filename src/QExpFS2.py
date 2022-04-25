15c0a13a8f36866e67f81930556b15cc67114787
YesunHuang <tgbus944812203@hotmail.com>
2022-04-26 00:42:38 +0800

    Update FS2

--- src/QExpFS2.py
+++ src/QExpFS2.py
@@ -1,6 +1,15 @@
 '''
-Name: QExpFM2
-Desriptption: Fine tuning using QExpF2
+Name: 
+Desriptption: 
+Email: yesunhuang@mail.ustc.edu.cn
+OpenSource: https://github.com/yesunhuang
+Msg: 
+Author: YesunHuang
+Date: 2022-04-26 00:42:23
+'''
+'''
+Name: QExpFS
+Desriptption: Full power 2 qubits with single sample
 Email: yesunhuang@mail.ustc.edu.cn
 OpenSource: https://github.com/yesunhuang
 Msg: Experiment One
@@ -66,7 +75,7 @@ if __name__=='__main__':
 
     # Load the network
 if LOAD_NETWORK and __name__=='__main__':
-    filename='QExpFM2.pt'
+    filename='QExpFS.pt'
     netData=torch.load(os.path.join(netSavepath,filename))
 
     inputSize=netData['inputSize']
@@ -75,6 +84,8 @@ if LOAD_NETWORK and __name__=='__main__':
     
     inputQubits=netData['inputQubits']
     outputQubits=netData['outputQubits']
+
+    isDensity=netData['isDensity']
     activation=netData['activation']
     
     interQPairs=netData['interQPairs']
@@ -82,9 +93,10 @@ if LOAD_NETWORK and __name__=='__main__':
     rescale=netData['rescale']
 
     sysConstants=netData['sysConstants']
-    measEffect=True
-    if not TRAIN_NETWORK:
-        sysConstants['numCpus']=1
+    measEffect=netData['measEffect']  
+    samples=netData['samples']
+
+    sysConstants['numCpus']=1
 
 elif __name__=='__main__':
     # Model
@@ -92,13 +104,15 @@ elif __name__=='__main__':
     inputSize=outputSize=1
     qubits=2
     activation=[0]
+    isDensity=False
     inputQubits=outputQubits=[i for i in range(qubits)]
     interQPairs=[[i,j] for i in range(qubits) for j in range(i+1,qubits)]
     rescale={'WIn':1,'J':torch.tensor([0.5])}
     inactive=[]
     sysConstants={'measureQuantity':'y','Dissipation':None,\
         'tau':4.0,'steps':3,'numCpus':16}
-    measEffect=True
+    samples=1
+    measEffect=False
 
 if __name__=='__main__':
     ## print parameters
@@ -111,13 +125,15 @@ if __name__=='__main__':
     ## Get neccesary functions
     srnnTestSup=QuantumSystemFunction()
     #transform=lambda Xs:[torch.squeeze(x) for x in Xs]
-    init_rnn_state=srnnTestSup.get_init_state_fun(activation=activation)
+    init_rnn_state=srnnTestSup.get_init_state_fun(activation=activation,\
+                                                isDensity=isDensity)
     get_params=srnnTestSup.get_get_params_fun(inputQubits=inputQubits,\
                                             outputQubits=outputQubits,\
                                             interQPairs=interQPairs,\
                                             inactive=inactive,\
                                             rescale=rescale)
-    rnn=srnnTestSup.get_forward_fn_fun(measEffect=measEffect,\
+    rnn=srnnTestSup.get_forward_fn_fun(samples=samples,\
+                                        measEffect=measEffect,\
                                         sysConstants=sysConstants)
     predict_fun=srnnTestSup.get_predict_fun(outputTransoform=transform)
 
@@ -131,7 +147,7 @@ if LOAD_NETWORK and __name__=='__main__':
 if __name__=='__main__':
     state=net.begin_state(batchSize)
     Y,newState=net(X,state)
-    print(Y.shape, len(newState), newState[0][0].shape)
+    print(Y.shape, len(newState), newState[0][0][0].shape)
 
 if not LOAD_NETWORK and not TRAIN_NETWORK:
     print('The network is not trained, are you sure to move on?')
@@ -141,10 +157,15 @@ if  TRAIN_NETWORK and __name__=='__main__':
     ## Parameters
     if LOAD_NETWORK:
         print('Are you sure to train the trained network?')
-    num_epochs= 100
-    maxLevyStepSize=[0.1]*5
-    regular=[2,1,5,2,1]
-    nestNum=40
+        num_epochs=netData['OptimizerConstant']['num_epochs']
+        maxLevyStepSize=netData['OptimizerConstant']['maxLevyStepSize']
+        regular=netData['OptimizerConstant']['regular']
+        nestNum=netData['OptimizerConstant']['nestNum']
+    else:
+        num_epochs= 300
+        maxLevyStepSize=[0.1]*5
+        regular=[2,1,5,2,1]
+        nestNum=40
     step_epochs=5
 
 ## Initial loss
@@ -193,16 +214,16 @@ if TRAIN_NETWORK and __name__=='__main__':
     ## Save the network
 if SAVE_NETWORK and __name__=='__main__':
     ## Parameters
-    filename='QExpFM2.pt'
+    filename='QExpFS.pt'
     OptimizerConstant={'num_epochs':num_epochs,'maxLevyStepSize':maxLevyStepSize,\
         'nestNum':nestNum}
     netData={'NetParams':net.params,'NetConstants':net.constants,\
             'inputSize':inputSize,'qubits':qubits,'outputSize':outputSize,\
-            'activation':activation,'isDensity':True,\
+            'activation':activation,'isDensity':isDensity,\
             'inputQubits':inputQubits,'outputQubits':outputQubits,\
             'interQPairs':interQPairs,'inactive':inactive,\
             'rescale':{},'isRandom':True,\
-            'sysConstants':sysConstants,'samples':1,'measEffect':measEffect,\
+            'sysConstants':sysConstants,'samples':samples,'measEffect':measEffect,\
             'Loss':l_epochs,'OptimizerConstant':OptimizerConstant}
     torch.save(netData,os.path.join(netSavepath,filename))
 
